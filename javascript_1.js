15e130b773a230dd26bc57f729d14198520301b5
Charlton Fernandes <charlton18fernandes@gmail.com>
2015-09-11 23:40:42 +0530

    (3)

--- javascript_1.js
+++ javascript_1.js
@@ -2,7 +2,11 @@
 /*
 	Multi Line Comment
 */
+
+document.getElementById("p3").innerHTML="Hello, From External JS.";
+
 function js_function_3() 
 {
+	// debugger;	//Stops execution of script untill play is pressed in debugger
     document.getElementById("p1").innerHTML = "You Want to See Me Again?";
 }
